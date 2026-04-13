403c613a9b1f540f1a9b9d4bf5f7246ba87f4bb8
senbet22 <senbet22@users.noreply.github.com>
2026-04-13 13:25:53 +0200

    Added List overloads. in Operations Class.

--- Calculator/Operations.cs
+++ Calculator/Operations.cs
@@ -3,6 +3,9 @@ namespace Calculator
 {
     public class Operations
     {
+        /// <summary>
+        /// Two number overloads.
+        /// 
         public double Add(double a, double b) => a + b;
 
         public double Subtract(double a, double b) => a - b;
@@ -15,5 +18,52 @@ namespace Calculator
                 throw new DivideByZeroException("Cannot divide by zero.");
             return a / b;
         }
+
+        /// <summary>
+        /// List overloads.
+        ///
+        public double Add(List<double> numbers)
+        {
+            double result = 0;
+            foreach (double num in numbers) 
+            {
+                result += num;
+            }
+            return result;
+        }
+
+        public double Subtract(List<double> numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                result -= numbers[i];
+            }
+            return result;
+        }
+
+        public double Multiply(List<double> numbers)
+        {
+            double result = 1;
+            foreach (double num in numbers) 
+            {
+                result *= num;
+            }
+            return result;
+        }
+
+        public double Divide(List<double> numbers)
+        {
+            double result = numbers[0];
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                if (numbers[i] == 0)
+                    throw new DivideByZeroException("Cannot divide by zero.");
+                result /= numbers[i];
+            }
+            return result;
+        }
     }
+
+
 }
