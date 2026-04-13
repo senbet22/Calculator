b169dc4ea001cc292be0d1d51f75445fd46e460e
senbet22 <senbet22@users.noreply.github.com>
2026-04-13 11:44:29 +0200

    Calculator Operation methods added.

--- Calculator/Operations.cs
+++ Calculator/Operations.cs
@@ -1,8 +1,19 @@
 ﻿
 namespace Calculator
 {
-    public class Calculator
+    public class Operations
     {
+        public double Add(double a, double b) => a + b;
 
+        public double Subtract(double a, double b) => a - b;
+
+        public double Multiply(double a, double b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Cannot divide by zero.");
+            return a / b;
+        }
     }
 }
